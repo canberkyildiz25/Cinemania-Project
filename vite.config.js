a11643a7b51741c78cc1ed2b8e5b312c5f5707bc
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2025-11-29 14:54:26 +0300

    dosya yapısı ve içeriği baştan düzenlendi

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,23 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import { glob } from "glob";
+import injectHTML from "vite-plugin-html-inject";
+
 export default defineConfig(({ command }) => {
   return {
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      [command === "serve" ? "global" : "_global"]: {},
     },
-    root: 'src',
+    root: "src", // DİKKAT: Kök dizini 'src' olarak ayarladık
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync(['./src/index.html', './src/pages/**/*.html']),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+        // src klasörü altındaki tüm html dosyalarını giriş noktası yap
+        input: glob.sync("./src/*.html"),
+        outDir: "../dist",
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    // base: '/CineCode/',
     plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-      viteStaticCopy({
-      targets: [
-        { src: 'components/**/*', dest: 'components' }, 
-      ],
-    }),
+      injectHTML(), // Bu eklenti <load> etiketlerini işler
     ],
   };
 });
